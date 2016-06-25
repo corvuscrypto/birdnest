6b079ceb426851a9f93a82441703912901257e2f
Clifford Richardson <cmrallen@gmail.com>
2016-06-25 14:36:17 -0700

    added encryption and decryption utility functions and added tests for the encryption of data

--- sessions/session.go
+++ sessions/session.go
@@ -5,10 +5,3 @@ type Session interface {
 	IsGuest() bool
 	IsValid() bool
 }
-
-//SessionProvider is the interface through which sessions are created or removed
-type SessionProvider interface {
-	NewSessionToken() []byte
-	RemoveSession()
-	RefreshSession()
-}
