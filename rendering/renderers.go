04cd6cd758cd481eb4e1b0c37c9232d309e99cac
Clifford Richardson <cmrallen@gmail.com>
2016-07-04 19:00:26 -0700

    Changed the structure of rendering flow

--- rendering/renderers.go
+++ rendering/renderers.go
@@ -1,3 +1,6 @@
+/*
+the rendering package contains several renderers for use with birdnest.
+*/
 package rendering
 
 import (
@@ -5,11 +8,6 @@ import (
 	"errors"
 
 	"github.com/corvuscrypto/birdnest/requests"
-)
-
-//These are the built-in renderers that are automatically supported
-const (
-	JSONRenderer string = "json"
 )
 
 //These are the rendering errors. The UnableToRender error can be used,
@@ -20,9 +18,42 @@ var (
 )
 
 //Renderer handles request contexts and marshals the data via HTTP. The Render method MUST throw an error if it is
-//unable to render the data and it SHOULD set the Response code to 500
+//unable to render the data and it SHOULD set the Response code to 500.
+//
+//Unlike many traditional renderers which use a view configuration or REQUIRE manual rendering, birdnest renderers instead
+//implement a NewView method which wraps a request handler and calls the renderers exported Render method
+//after data has been handled. You may still call render manually if you choose. Multiple calls to Render should
+//do nothing.
 type Renderer interface {
+	NewView(func(*requests.Request)) func(*requests.Request) error
 	Render(*requests.Request) error
+}
+
+//DefaultRenderer is the default struct which satisfies the Renderer interface and allows manipulation of rendering
+//behavior via mutation of the renderFunc struct member. NOTE: because this can be changed at runtime I'm sure there
+//are a few cool tricks to be implemented, but I'd recommend against them. Changing renderer behavior on the fly can
+//make for annoying debugging and also black holes that emit nad-punching unicorns will appear for like... no reason!
+type DefaultRenderer struct {
+	renderFunc func(*requests.Request) error
+}
+
+//NewView on the DefaultRenderer type satisfied the Renderer interface and wraps a request handler such that rendering
+//occurs at the final stage of request handling. For the DefaultRenderer, if there is an error during rendering, the
+//response will have a 500 code written to it.
+func (d *DefaultRenderer) NewView(f func(*requests.Request)) func(*requests.Request) {
+	return func(r *requests.Request) {
+		f(r)
+		err := d.renderFunc(r)
+		if err != nil {
+			r.Response.WriteHeader(500)
+		}
+	}
+}
+
+//Render on the DefaultRenderer type satisfies the Renderer interface and allows direct rendering of
+//a web response
+func (d *DefaultRenderer) Render(r *requests.Request) error {
+	return d.renderFunc(r)
 }
 
 //this is for private access only
@@ -39,28 +70,16 @@ func GetRenderer(name string) (Renderer, error) {
 
 //JSONRenderer transforms a request context into a JSON Response. If the renderer fails to process it will
 //automatically set the header to an error 500 code.
-type jsonRenderer struct{}
-
-//Render renders the context within a request to a proper JSON response
-func (r *jsonRenderer) Render(req *requests.Request) error {
-	data, err := json.Marshal(req.Ctx)
-	if err != nil {
-		req.Response.WriteHeader(500)
-		return err
-	}
-	//ensure that the content header is set
-	req.Response.Header().Set("Content-Type", "application/json")
-	req.Response.Write(data)
-	return nil
-}
-
-//RegisterRenderer adds a renderer to the registry for use
-func RegisterRenderer(name string, renderer Renderer) {
-	availableRenderers[name] = renderer
-}
-
-func init() {
-	availableRenderers = make(map[string]Renderer)
-	//add the default renderers to the availableRenderers map
-	availableRenderers[JSONRenderer] = new(jsonRenderer)
+var JSONRenderer = &DefaultRenderer{
+	func(req *requests.Request) error {
+		data, err := json.Marshal(req.Ctx)
+		if err != nil {
+			req.Response.WriteHeader(500)
+			return err
+		}
+		//ensure that the content header is set
+		req.Response.Header().Set("Content-Type", "application/json")
+		req.Response.Write(data)
+		return nil
+	},
 }
