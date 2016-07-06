80258cc85f5167f2f8ef6ff427e7f899edd3b0f0
Clifford Richardson <cmrallen@gmail.com>
2016-07-05 21:12:29 -0700

    reverted back to the exported type with documentation forbidding manual instantiation

--- server/router.go
+++ server/router.go
@@ -12,23 +12,9 @@ import (
 //order to register a new middleware request handler
 type RequestHandler func(*requests.Request)
 
-//Router is the interface which handles route registration. While you may utilize your own router, it is strongly
-//recommended that you utilize the factory method NewRouter in order to create a router instance. A router must
-//implement a static ServeFiles method and the request-method route registration function members as documented.
-type Router interface {
-	http.Handler
-	Handle(method, path string, handle RequestHandler, renderer rendering.Renderer)
-	OPTIONS(path string, handle RequestHandler, renderer rendering.Renderer)
-	GET(path string, handle RequestHandler, renderer rendering.Renderer)
-	POST(path string, handle RequestHandler, renderer rendering.Renderer)
-	PATCH(path string, handle RequestHandler, renderer rendering.Renderer)
-	DELETE(path string, handle RequestHandler, renderer rendering.Renderer)
-	HEAD(path string, handle RequestHandler, renderer rendering.Renderer)
-	PUT(path string, handle RequestHandler, renderer rendering.Renderer)
-	ServeFiles(path string, root http.FileSystem)
-}
-
-type router struct {
+//Router is an adapter for the httprouter.Router. DO NOT INSTANTIATE THIS MANUALLY!
+//This struct is exported only for documentation purposes and you must use the factory method provided (NewRouter)
+type Router struct {
 	router       *httprouter.Router
 	PanicHandler RequestHandler
 	NotFHandler  RequestHandler
@@ -43,13 +29,13 @@ func transformRequest(w http.ResponseWriter, r *http.Request, p httprouter.Param
 	return req
 }
 
-func (r *router) panicHandler() {
+func (r *Router) panicHandler() {
 	if p := recover(); p != nil {
 
 	}
 }
 
-func (r *router) wrapHandler(h RequestHandler, renderer rendering.Renderer) httprouter.Handle {
+func (r *Router) wrapHandler(h RequestHandler, renderer rendering.Renderer) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		req := transformRequest(w, r, p)
 		applyMiddleware(req)
@@ -61,68 +47,68 @@ func (r *router) wrapHandler(h RequestHandler, renderer rendering.Renderer) http
 }
 
 //Handle is the adapter for birdnest routing to utilize httprouter's Handle method
-func (r *router) Handle(method, path string, handle RequestHandler, renderer rendering.Renderer) {
+func (r *Router) Handle(method, path string, handle RequestHandler, renderer rendering.Renderer) {
 	r.router.Handle(method, path, r.wrapHandler(handle, renderer))
 }
 
 //OPTIONS is the adapter for the httprouter OPTIONS shortcut.
 //If renderer is not nil then the request will be rendered using that rendering agent
-func (r *router) OPTIONS(path string, handle RequestHandler, renderer rendering.Renderer) {
+func (r *Router) OPTIONS(path string, handle RequestHandler, renderer rendering.Renderer) {
 	r.router.Handle("OPTIONS", path, r.wrapHandler(handle, renderer))
 }
 
 //PATCH is the adapter for the httprouter PATCH shortcut.
 //If renderer is not nil then the request will be rendered using that rendering agent
-func (r *router) PATCH(path string, handle RequestHandler, renderer rendering.Renderer) {
+func (r *Router) PATCH(path string, handle RequestHandler, renderer rendering.Renderer) {
 	r.router.Handle("PATCH", path, r.wrapHandler(handle, renderer))
 }
 
 //POST is the adapter for the httprouter POST shortcut.
 //If renderer is not nil then the request will be rendered using that rendering agent
-func (r *router) POST(path string, handle RequestHandler, renderer rendering.Renderer) {
+func (r *Router) POST(path string, handle RequestHandler, renderer rendering.Renderer) {
 	r.router.Handle("POST", path, r.wrapHandler(handle, renderer))
 }
 
 //PUT is the adapter for the httprouter PUT shortcut.
 //If renderer is not nil then the request will be rendered using that rendering agent
-func (r *router) PUT(path string, handle RequestHandler, renderer rendering.Renderer) {
+func (r *Router) PUT(path string, handle RequestHandler, renderer rendering.Renderer) {
 	r.router.Handle("PUT", path, r.wrapHandler(handle, renderer))
 }
 
 //GET is the adapter for the httprouter GET shortcut.
 //If renderer is not nil then the request will be rendered using that rendering agent
-func (r *router) GET(path string, handle RequestHandler, renderer rendering.Renderer) {
+func (r *Router) GET(path string, handle RequestHandler, renderer rendering.Renderer) {
 	r.router.Handle("GET", path, r.wrapHandler(handle, renderer))
 }
 
 //DELETE is the adapter for the httprouter DELETE shortcut.
 //If renderer is not nil then the request will be rendered using that rendering agent
-func (r *router) DELETE(path string, handle RequestHandler, renderer rendering.Renderer) {
+func (r *Router) DELETE(path string, handle RequestHandler, renderer rendering.Renderer) {
 	r.router.Handle("DELETE", path, r.wrapHandler(handle, renderer))
 }
 
 //HEAD is the adapter for the httprouter HEAD shortcut.
 //If renderer is not nil then the request will be rendered using that rendering agent
-func (r *router) HEAD(path string, handle RequestHandler, renderer rendering.Renderer) {
+func (r *Router) HEAD(path string, handle RequestHandler, renderer rendering.Renderer) {
 	r.router.Handle("HEAD", path, r.wrapHandler(handle, renderer))
 }
 
 //ServeFiles is wrapper to set a static fileserver route onto the underlying httprouter.Router instance
-func (r *router) ServeFiles(path string, root http.FileSystem) {
+func (r *Router) ServeFiles(path string, root http.FileSystem) {
 	r.router.ServeFiles(path, root)
 }
 
-func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	r.router.ServeHTTP(w, req)
 }
 
 //NewRouter returns a Router instance. If an *httprouter.Router instance is passed into NewRouter, the Router uses
 //it, otherwise if it is nil the default is used
-func NewRouter(r *httprouter.Router) Router {
+func NewRouter(r *httprouter.Router) *Router {
 	if r == nil {
 		r = httprouter.New()
 	}
-	ret := new(router)
+	ret := new(Router)
 	ret.router = r
 	return ret
 }
